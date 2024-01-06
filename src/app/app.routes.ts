945d757f2411308a13fd4be579d3fa7f0dc16b74
Harry Morgan <harrydouglasmorgan@gmail.com>
2024-01-06 21:15:09 +0000

    Added new components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SignInComponent } from './login/sign-in/sign-in.component';
+import { SignUpComponent } from './login/sign-up/sign-up.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'signin', component: SignInComponent },
+  { path: 'signup', component: SignUpComponent },
+];

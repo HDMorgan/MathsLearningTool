98f37cd5546e09d10f81a7ff0d6d4f4c8707f504
Harry Morgan <harrydouglasmorgan@gmail.com>
2024-02-14 00:54:15 +0000

    Fixed test

--- src/app/join/join.component.spec.ts
+++ src/app/join/join.component.spec.ts
@@ -1,23 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { JoinComponent } from './join.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('JoinComponent', () => {
-  let component: JoinComponent;
-  let fixture: ComponentFixture<JoinComponent>;
+	let component: JoinComponent;
+	let fixture: ComponentFixture<JoinComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [JoinComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(JoinComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [JoinComponent, BrowserAnimationsModule],
+		}).compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+		fixture = TestBed.createComponent(JoinComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });

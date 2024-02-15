2bebb752ec59ecbda8225c82802030d8aaf2bcb6
Harry Morgan <harrydouglasmorgan@gmail.com>
2024-02-15 19:36:42 +0000

    Changed layout for mobile

--- src/app/login/login-form/login-form.component.ts
+++ src/app/login/login-form/login-form.component.ts
@@ -1,20 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+	FormBuilder,
+	FormGroup,
+	ReactiveFormsModule,
+	Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
 
 @Component({
 	selector: 'app-login-form',
 	standalone: true,
-	imports: [],
+	imports: [
+		MatButtonModule,
+		MatFormFieldModule,
+		MatInputModule,
+		ReactiveFormsModule,
+		CommonModule,
+		RouterLink,
+	],
 	templateUrl: './login-form.component.html',
 	styleUrl: './login-form.component.css',
 })
 export class LoginFormComponent {
-	private loginForm: FormGroup;
+	loginForm: FormGroup;
 
 	constructor(private formBuilder: FormBuilder) {
 		this.loginForm = formBuilder.group({
 			email: ['', [Validators.required, Validators.email]],
 			password: ['', [Validators.required, Validators.minLength(6)]],
 		});
+	}
+
+	OnSubmit() {
+		console.log('Email:' + this.loginForm.value[0]);
 	}
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,11 +3,17 @@ import { IconRegisterService } from './../services/mat-icon-register.service';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @Component({
 	selector: 'app-login',
 	standalone: true,
-	imports: [MatButtonModule, MatIconModule, HttpClientModule],
+	imports: [
+		MatButtonModule,
+		MatIconModule,
+		HttpClientModule,
+		LoginFormComponent,
+	],
 	templateUrl: './login.component.html',
 	styleUrl: './login.component.css',
 })

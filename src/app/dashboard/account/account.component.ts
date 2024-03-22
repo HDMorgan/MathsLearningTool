a60314afc20539d3cb80bf79056c4ff28dee3db6
Harry Morgan <harrydouglasmorgan@gmail.com>
2024-03-22 16:14:26 +0000

    Started work

--- src/app/dashboard/account/account.component.ts
+++ src/app/dashboard/account/account.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
-  selector: 'app-account',
-  standalone: true,
-  imports: [],
-  templateUrl: './account.component.html',
-  styleUrl: './account.component.scss'
+	selector: 'app-account',
+	standalone: true,
+	imports: [MatButtonModule, MatInputModule],
+	templateUrl: './account.component.html',
+	styleUrl: './account.component.scss',
 })
-export class AccountComponent {
-
-}
+export class AccountComponent {}
